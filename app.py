b3db97a5bff83b538ab1724f1789c05e4c95b0d4
halevialon <halevialon@gmail.com>
2021-12-21 19:34:13 +0200

    Assignment 8

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
 @app.route('/home')
 def helloWorld():  # put application's code here
     return 'Hey everyone!'
@@ -22,6 +21,25 @@ def goToArticles():
 @app.route('/AlonHalevi')
 def halevialon():
     return redirect(url_for('goToArticles'))
+
+
+@app.route('/')
+def home():
+    return render_template('CV1.html')
+
+
+@app.route('/contact_us_page')
+def contact_us():
+    return render_template('CV2.html')
+
+
+@app.route('/Assignment8')
+def assignment_8_page():
+    first_name = 'Alon'
+    last_name = 'Halevi'
+    return render_template('assignment8.html', name=first_name, family_name=last_name,
+                           hobbies=('Cheering for Phili76ers', 'Playing Basketball`', 'Hearing Music'))
+
 
 
 if __name__ == '__main__':
